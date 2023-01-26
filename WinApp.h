d6c9f418330a302e5aeecea1af0b0582668ce03b
oumi0310 <k021g1382@g.neec.ac.jp>
2023-01-26 15:52:45 +0900

    完了

--- WinApp.h
+++ WinApp.h
@@ -7,8 +7,20 @@ class WinApp
 public:
 	static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
+	static const int window_width = 1280;
+
+	static const int window_height = 720;
+
 public:
 	void Initialize();
 
 	void Update();
+
+	HWND GetHwnd() const { return hwnd; }
+	HINSTANCE GetHInstace() const { return w.hInstance; }
+
+private:
+	HWND hwnd = nullptr;
+
+	WNDCLASSEX w{};
 };
