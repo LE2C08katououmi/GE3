24d9ae6059d41ae14ee783f1d516e2de5ed1a557
oumi0310 <k021g1382@g.neec.ac.jp>
2023-01-27 14:15:16 +0900

    04_02まで終わった

--- DirectXCommon.h
+++ DirectXCommon.h
@@ -2,6 +2,7 @@
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <wrl.h>
+#include <vector>
 
 #include "WinApp.h"
 
@@ -16,9 +17,29 @@ public:
 
 	void InitializeSwapchain();
 
+	void InitializeRenderTargetView();
+
+	void InitializeDepthBuffer();
+
+	void InitializeFence();
+
 private:
 	Microsoft::WRL::ComPtr<ID3D12Device> device;
 	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	// デスクリプタヒープの設定
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+
+	std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> backBuffers;
+
+	ComPtr<ID3D12Device> device;
+	ComPtr<IDXGIFactory7> dxgiFactory;
+	ComPtr<IDXGISwapChain4> swapChain;
+	ComPtr<ID3D12CommandAllocator> commandAllocator;
+	ComPtr<ID3D12GraphicsCommandList> commandList;
+	ComPtr<ID3D12CommandQueue> commandQueue;
+	ComPtr<ID3D12DescriptorHeap> rtvHeap;
 
 	WinApp* winApp = nullptr;
 };
